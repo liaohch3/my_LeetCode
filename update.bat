0bc8bf0012b88bbb7d9c04ceb96c135a2c09d0d3
liaohch3 <liaohch3@mail2.sysu.edu.cn>
2020-01-20 00:14:16 +0800

    20202001415

--- update.bat
+++ update.bat
@@ -1,1 +1,11 @@
-echo %date%
+@echo off
+
+set hour=%time:~,2%
+if "%time:~,1%"==" " set hour=0%time:~1,1%
+
+echo %date:~0,4%%date:~5,2%%date:~8,2%%hour%%time:~3,2%%time:~6,2%
+
+git add .
+git commit -m %date:~0,4%%date:~5,2%%date:~8,2%%hour%%time:~3,2%%time:~6,2%
+git push origin master
+pause
