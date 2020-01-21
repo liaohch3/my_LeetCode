f3ab43bf89a8b650564b1889b8c6a42eea44c0d4
liaohch3 <liaohch3@mail2.sysu.edu.cn>
2020-01-21 22:30:31 +0800

    2020-01-21

---
+++ 数据结构专题/队列&栈/钥匙和房间.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+
+	set<int> visited;
+
+    bool canVisitAllRooms(vector<vector<int> >& rooms) {
+    	visited.insert(0);
+    	DFS(rooms, 0);
+    	return visited.size() == rooms.size();
+    }
+
+	void DFS(vector<vector<int> >& rooms, int x)
+	{
+		for(int i = 0; i < rooms[x].size(); i++)
+		{
+			if(visited.find(rooms[x][i]) == visited.end())
+			{
+				visited.insert(i);
+				DFS(rooms, i);
+			}
+		}
+	}
+
+};
+
+int main()
+{
+
+	return 0;
+}
